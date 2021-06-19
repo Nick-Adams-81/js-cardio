b8787bd6664cddc974d489cd6c0d16ebea4ecfe0
nick adams <thebigdeal916@icloud.com>
2021-06-19 11:31:52 -0500

    fizz buzz added

--- cardio.js
+++ cardio.js
@@ -47,9 +47,78 @@ function isPalendrome(str) {
 function reverseInt(int) {
     const revString = int.toString().split('').reverse().join('')
     return parseInt(revString) * Math.sign(int)
+}
+
+/////////////finding capitalized letters///////////////
+function capLetter(str) {
+//es-5 for loop
+    //const strArr = str.toLowerCase().split(' ')
+
+    //for(let i = 0; i < strArr.length; i++) {
+        //strArr[i] = strArr[i].substring(0, 1).toUpperCase() + 
+        //strArr[i].substring(1)
+    //}
+    //return strArr.join(' ')
+//es-6 map 
+    //return str
+        //.toLowerCase()
+        //.split(' ')
+        //.map(word => word[0].toUpperCase() + word.substr(1))
+        //.join(' ')
+
+// regular expressions
+    //return str.replace(/\b[a-z]/gi, function(char) {
+        //return char.toUpperCase()
+    //})
 
 }
 
+///////////finding max characters in a string////////////
+function maxCharacters(str) {
+    const charMap = {}
+    let maxNum = 0
+    let maxChar = ''
 
-const output = reverseInt(1234)
+    str.split('').forEach(function(char) {
+        if(charMap[char]){
+            charMap[char]++
+        } else {
+            charMap[char] = 1
+        }
+    })
+
+    for(let char in charMap) {
+        if(charMap[char] > maxNum ) {
+            maxNum = charMap[char]
+            maxChar = char
+        } 
+    }
+    return maxChar
+
+}
+
+///////////////fizzbuzz/////////////
+
+function fizzBuzz() {
+    for(let i = 1; i <= 100; i++) {
+        if(i % 15 === 0) {
+            console.log('fizz buzz')
+        } else if( i % 3 === 0 ) {
+            console.log('fizz')
+
+        } else if(i % 5 === 0) {
+            console.log('buzz')
+
+        } else {
+            console.log(i)
+        }
+
+    }
+}
+
+
+
+
+
+const output = fizzBuzz()
 console.log(output)
